e683a7cdd12d16a9655259d3e18ea6f540dc9b30
Lake-232434 <2569157506@qq.com>
2018-11-29 22:02:01 +0800

    '写了点bug'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ignoreRouter = require('./config/ignoreRouter');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+  if(ignoreRouter.indexOf(req.url) > -1){
+    next();
+  }
+
+  var nickname=req.cookies.nickname;
+  if(nickname){
+    next();
+  }else{
+    res.redirect('/login.html');
+  }
+})
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/login.html', function(req, res, next) {
+  res.render('login');
+});
+
+// router.get('/users.html', function(req,res){
+//   res.render('users');
+// });
+router.get('/brand.html', function(req,res){
+  res.render('brand');
+});
+router.get('/phone.html', function(req,res){
+  res.render('phone');
+});
+
 module.exports = router;
